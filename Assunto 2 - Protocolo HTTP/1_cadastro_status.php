2b9492cdad7cd0dee241619ad22626769b285fe1
victoria6-v <victoria.g.santos6@edu.senai.br>
2026-08-18 16:11:34 -0300

    assunto 2
    inclusão do material

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>cadastro - Status Codes</title>
+</head>
+<body>
+    
+<h1>cadastro de aluno(com status code)</h1>
+<form action="nome">Nome:</label>
+<input tipe="text" name="nome" required><br><br>
+<form action="idade">idade:</label>
+<input tipe="text" name="nome" required><br><br>
+
+<button type="submit">Enviar</button>
+
+</form>
+
+
+
+
+</body>
+</html>
---
+++ Assunto 2 - Protocolo HTTP/exercício/2_cadastro_livros.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Livros</title>
+</head>
+<body>
+
+    <h1>Cadastro de Livros</h1>
+
+    <form method="post" action="">
+        <label for="titulo">Título:</label>
+        <input type="text" name="titulo" required><br><br>
+
+        <label for="ano">Ano:</label>
+        <input type="text" name="ano" required><br><br>
+
+        <button type="submit">Enviar</button>
+    </form>
+
+    <hr>
+
+    <?php
+
+    // Monte seu código PHP (Aqui)
+
+    ?>
+
+</body>
+</html>
